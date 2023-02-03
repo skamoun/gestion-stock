48bf0521f0857929c09cb32494e8d321ed11c162
S.KAMOUN <said.kamoun@gmail.com>
2023-02-03 16:43:09 +0100

    add columns

--- src/main/java/com/kamoun/gestiondestock/model/CommandeClient.java
+++ src/main/java/com/kamoun/gestiondestock/model/CommandeClient.java
@@ -1,10 +1,12 @@
 package com.kamoun.gestiondestock.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Data
 @Entity
@@ -12,4 +14,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Table(name = "commande_client")
 public class CommandeClient extends AbstractEntity{
+    private static final String CLIENT_JOIN_COLUMN ="_id_client";
+    private String code;
+
+    private LocalDate dateCommande;
+    @ManyToOne
+    @JoinColumn(name = CLIENT_JOIN_COLUMN)
+    private Client client;
+
+    private List<LigneCommandeClient> ligneCommandeClients;
 }
